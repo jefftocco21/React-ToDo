8469bb8f84a6ab3b5d656928ff02cc74babcd6a2
jnt369@gmail.com <jeff.tocco@lyonsexpress.com>
2022-03-23 10:43:48 -0400

    javacsript to create CSS rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
 import './App.css';
 import Header from './components/Header'
+import propTypes from 'prop-types'
 
 function App() {
 
   return (
     <div className="Container">
-        <header className="App-header">
+        <header 
+            style={headingStyle}
+            className="App-header">
           <Header 
+
           title="To Do"
           />
         </header>
     </div>
   );
+}
+
+//can use javascript to create CSS rules
+// const headingStyle = {
+//   color:'red', 
+//   backgroundColor: 'black'
+// }
+
+Header.propTypes = {
+  title: propTypes.string.isRequired
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
-const Header = ({title}) => {
+const Header = ({title}) => { //accepting props, specifcially title defined in App.jsx
   return (
     <header>
-        <h1>
-           {title}
+        <h1> 
+           {title} 
         </h1>
     </header>
   )
