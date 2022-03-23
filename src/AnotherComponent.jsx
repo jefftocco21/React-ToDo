535361286be1205fe7a795d74795187aac508e6c
jnt369@gmail.com <jeff.tocco@lyonsexpress.com>
2022-03-23 10:36:03 -0400

    Getting rid of starting bloat, adding title prop to be passed

--- src/AnotherComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Another(props){
-    return(
-        <div>
-            Another Component, by {props.name}
-        </div>
-    )
-}
--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-import AnotherComponent from './AnotherComponent';
+import Header from './components/Header'
 
 function App() {
-  //count, setter for the piece of state, and initial value of 0 using useState 'hook"
-  const [count, setCount] = useState(0);
-
-  function decrement() {
-    // setCount(count-1);
-    setCount(prevCount => prevCount-1)
-  }
-
-  function increment(){
-    // setCount(count+1);
-    setCount(prevCount => prevCount+1)
-
-  }
-
-  const someStyle = {
-    background: 'blue',
-    color: 'white',
-    fontSize: '28px',
-    fontWeight: 'bold'
-  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <AnotherComponent name="Jeff"/>
-        <div>
-          <button onClick={decrement}>-</button>
-          <span>{count}</span>
-          <button onClick={increment}>+</button>
-        </div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p style = {someStyle}>{3+2} </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+        <header className="App-header">
+          <Header 
+          title="To Do"
+          />
+        </header>
     </div>
   );
 }
