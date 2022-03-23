935a05caa63317b2be037f815288c725f81e8c26
jnt369@gmail.com <jeff.tocco@lyonsexpress.com>
2022-03-23 15:13:34 -0400

    Creating AddTask component form UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 import { useState } from "react";
 
 const App = () => {
@@ -37,6 +38,7 @@ const toggleReminder = (id) => {
     return (
       <div className="container">
         <Header />
+        <AddTask />
         {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No active tasks!'}
       </div>
     );
